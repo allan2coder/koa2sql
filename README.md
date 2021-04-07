380e5075998f1aa4547178da55402bc691178014
xushunfa459 <xushunfa459@hellobike.com>
2021-04-07 18:55:00 +0800

    feat: first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# node2sql cli
+
+## 介绍
+node2sql.js 是一款高性能、高扩展性、功能强大的 Node.js 服务端框架。可拓展为 Web 端框架。
+
+## 特性
+- 基于 TypeScript 开发
+- 面向切面编程（AOP）
+- 包含脚本轮训
+- 提供 ORM、验证器、资源层、服务层、中间层等抽象层能力
+
+## 快速开始
+安装工具
+```bash
+$ npm install -g node2sql
+```
+
+创建工程
+```
+$ node2sql create example
+$ cd example
+$ npm start
+```
---
+++ src/lib/printer.ts
@@ -0,0 +1,14 @@
+export class Printer {
+  static power() {
+    console.log('');
+    console.log('');
+    console.log('        ____  ___');
+    console.log('       / __ \\/   ');
+    console.log('      / / / / /| | ');
+    console.log('     / /_/ / ___ ');
+    console.log('    /_____/_/  |_/');
+    console.log('');
+    console.log('');
+    console.log('Visit https://123.hellobike.com/ to learn more.');
+  }
+}
